330161e51f47e35c31c66e87ec1c0668e4c36de2
Oleg Grenrus <oleg.grenrus@iki.fi>
2019-10-23 20:49:40 +0300

    Fix build on macOS

--- test/TestProcess.hs
+++ test/TestProcess.hs
@@ -13,11 +13,11 @@ import qualified Data.ByteString as BS
 import Lukko
 
 #ifdef HAS_OFD_LOCKING
-import qualified Lukko.FLock as FLock
+import qualified Lukko.OFD as OFD
 #endif
 
 #ifdef HAS_FLOCK
-import qualified Lukko.OFD as OFD
+import qualified Lukko.FLock as FLock
 #endif
 
 main :: IO ()
